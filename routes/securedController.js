0772563f21a601a99f00ff9a37e38f9fb0e82284
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-04 00:55:13 +0530

    init

---
+++ routes/securedController.js
@@ -0,0 +1,10 @@
+const authMiddleware = require('../middleware/authMiddleware');
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/secured', authMiddleware, (req, res) => {
+  res.send('This is a secured route');
+});
+
+module.exports = router;
