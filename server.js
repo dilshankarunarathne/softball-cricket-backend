f7714f948d12aa094cf8ff678eb4b4dabc54ce72
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-04 02:03:21 +0530

    rem template code

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const app = express();
 const db = require('./db');
 
 const authController = require('./authController');
-const securedController = require('./securedController');
 
 app.use(cors()); 
 app.use(express.json());
 
 app.use('/auth', authController);
-app.use('/secured', securedController);
 
 app.listen(3000, () => console.log('Server started on port 3000'));
