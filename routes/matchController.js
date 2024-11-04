8d99f016b84de95bbabeef1b3049b79c6049fa7a
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-05 05:24:37 +0530

    bugfix: form data, import errors

--- routes/matchController.js
+++ routes/matchController.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./User');
+const User = require('../models/User');
+const Match = require('../models/Match');
 const multer = require('multer');
 const upload = multer();
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/', authMiddleware, async (req, res) => {
+router.post('/', authMiddleware, upload.none(), async (req, res) => {
     const { team1, team2, date, start_time, end_time, location } = req.body;
     const token = req.headers.authorization.split(' ')[1];
 
@@ -15,12 +16,13 @@ router.post('/', authMiddleware, async (req, res) => {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         if (decoded.user_type !== 'admin') {
             return res.status(403).send('Only admins can create matches');
-        }
+        } 
 
         const match = new Match({ team1, team2, date, start_time, end_time, location });
         await match.save();
         res.sendStatus(201);
     } catch (error) {
+        console.log(error);
         res.status(500).send('Internal server error');
     }
 });
@@ -38,7 +40,7 @@ router.get('/:id', async (req, res) => {
     res.send(match);
 });
 
-router.put('/:id', authMiddleware, async (req, res) => {
+router.put('/:id', authMiddleware, upload.none(), async (req, res) => {
     const { team1, team2, date, start_time, end_time, location } = req.body;
     const token = req.headers.authorization.split(' ')[1];
 
@@ -67,7 +69,7 @@ router.put('/:id', authMiddleware, async (req, res) => {
     }
 });
 
-router.delete('/:id', authMiddleware, async (req, res) => {
+router.delete('/:id', authMiddleware, upload.none(), async (req, res) => {
     const token = req.headers.authorization.split(' ')[1];
 
     try {
