288414f7b7ac5d4f17a328e2e874440009ffa73a
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-05 05:24:58 +0530

    refactor code style

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 const app = express();
 const db = require('./db');
 
-const authController = require('./authController');
-
 app.use(cors()); 
 app.use(express.json());
 
-app.use('/auth', authController);
+app.use('/auth', require('./routes/authController'));
+app.use('/admin', require('./routes/adminController'));
+app.use('/matches', require('./routes/matchController'));
+app.use('/teams', require('./routes/teamController'));
 
 app.listen(3000, () => console.log('Server started on port 3000'));
