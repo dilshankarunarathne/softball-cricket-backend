48125fe191ea09c3876fd7edc185a09d49ecc8ed
dilshankarunarathne <maleeshasparrow@gmail.com>
2024-11-09 12:43:39 +0530

    add score route

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.use('/auth', require('./routes/authController'));
 app.use('/admin', require('./routes/adminController'));
 app.use('/matches', require('./routes/matchController'));
 app.use('/teams', require('./routes/teamController'));
+app.use('/score', require('./routes/scoreController'));
 
 app.listen(3000, () => console.log('Server started on port 3000'));
